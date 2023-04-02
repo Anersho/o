4f9aa365b6869e11f08c8c0a572c561280abfdc7
Anersho <108976752+Anersho@users.noreply.github.com>
2023-04-02 13:11:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
+
 const fs = require('fs')
 const chalk = require('chalk')
 
 //aumto functioner
 global.autoTyping = true //auto tying in gc (true to on, false to off)
-global.autoRecord = true //auto recording (true to on, false to off)
+global.autoRecord = ture //auto recording (true to on, false to off)
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
@@ -14,33 +15,32 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['918714093544'] //ur owner number
+global.ownername = "Anersh" //ur owner name
+global.ytname = "RC WIZARD" //ur yt chanel name
+global.socialm = "INSTAGRAM: _.anersh.-" //ur github or insta name
+global.location = "India, kerala" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213'] //ur number
-global.ownernomer = "916909137213" //ur number
-global.ownertag = '916909137213' //ur tag number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot V5' //ur bot name
-global.ownername = "🦄Dream Guy Xeon"
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD5' //script link
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.ownernomer = "918714093544" //ur number
+global.premium = ['918714093544'] //ur premium number
+global.botname = 'BOT RC WIZARD' //ur bot name
+global.linkz = "https://instagram.com/_.anersh._?igshid=ZDdkNTZiNTM=" //your theme url which will be displayed on whatsapp
+global.websitex = "https://instagram.com/_.anersh._?igshid=ZDdkNTZiNTM=" //ur website to be displayed
+global.botscript = 'https://instagram.com/_.anersh._?igshid=ZDdkNTZiNTM=' //script link
+global.themeemoji = "😟" //ur theme emoji
+global.packname = "RC WIZARD" //ur sticker watermark packname
+global.author = "Cheems Bot\n\n\nA whatsapp bot developed by\n🦄Dream Guy Xeon\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213" //ur sticker watermark author
+global.wm = "RC WIZARD Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
+
+//messages
 global.mess = {
-    success: 'Done✓',
+    success: 'Here you go!',
     admin: 'This feature could be used by admins only!',
     botAdmin: 'Bot Must Be Admin First!',
     premime: 'Premium Special Features If You Want to Register Type Rent',
@@ -53,29 +53,73 @@ global.mess = {
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
 }
-global.limitawal = {
-    premium: "Infinity",
-    free: 25
-}
+
+//media target
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
 global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+
+//menu image maker
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
-//module api
-module.exports = {
-api:{
-     removebg: ['zNKnayDFH1nugtA81fkPMzXn','5CyygkXiT5vrki2Z6ZsUCE8u','Mz4yJkagrCLotdgsr4Ms39ud','vEeWnzQws9kJoYNMYKhbT1s6','2arViktax9HUdMqy9U7wFLKT','sfZpRHNwVPAG57nZVHijYZ9v','oqVVyQ2rBDYdUrxThg4GdjhA','rGp4axHpQ56Y5tRLX7J789QC','NfPx6NgTkpVYLnKUZHCAM1P3'],//https://remove.bg/api
-     unscreen: ['N6J4Bjbyh2V4eqhAPTWYtFER','fcKJkPstNXp4pjntYt3bR38E'],
-     upscaling: '1255173112',
-     imgsuper: ['198f69d4b2msh0021bb0690669a6p1f3a80jsn9cab1ae485cc','85731a45bbmshf7bd86f09b300c2p14e544jsncd18a8d5dba2'],//https://super-image1.p.rapidapi.com/
-     speechtotext: ['897beebb3ac74ceeaa6f8d0903b2297a']
-   }
-   }
+//adventure1
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+
+global.limitAwal = {
+ prem: 'Unlimited',
+ free: 70
+}
+
+//adventure2
+global.emot = {
+role: '🏆',
+level: '🎚️',
+limit: '📊',
+health: '❤️',
+exp: '💫',
+money: '💵',
+potion: '🥤',
+diamond: '💎',
+common: '📦',
+uncommon: '🎁',
+mythic: '🗳️',
+legendary: '🗃️',
+pet: '🎁',
+trash: '🗑',
+armor: '👕',
+sword: '⚔️',
+wood: '🪵',
+batu: '🪨',
+string: '🕸️',
+horse: '🐎',
+cat: '🐈',
+dog: '🐕',
+fox: '🦊',
+petFood: '🍖',
+iron: '⛓️',
+gold: '👑',
+emerald: '💚',
+budak: '🏃',
+busur: '🏹',
+panah: '💘',
+kapak: '🪓'
+}
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
